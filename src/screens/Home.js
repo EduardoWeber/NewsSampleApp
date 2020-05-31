69f32e67394daeb394a8098fd1eadb68f8172147
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-30 21:16:38 -0300

    First implementation of Home screen - FlatList with NewsItem's - Search function

---
+++ src/screens/Home.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import NewsItem from '../components/NewsItem';
+import Search from '../components/Search';
+
+const DATA = [
+  {
+    author: 'Fulano',
+    date: 'Hoje',
+    title: 'Titulo da noticia',
+  },
+  {
+    author: 'Fulano',
+    date: 'Hoje',
+    title: 'Titulo muito bom',
+  },
+  {
+    author: 'Fulano',
+    date: 'Hoje',
+    title: 'Titulo muito super longo de teste longo pra ver quanto que vai dar',
+  },
+  {
+    author: 'Fulano',
+    date: 'Hoje',
+    title: 'Titulo noticia',
+  },
+];
+
+export default function Home({ navigation }) {
+  const [filteredList, setFilteredList] = useState(DATA);
+
+  function filterList(title) {
+    const newList = DATA.filter((item) => {
+      return item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase());
+    });
+    setFilteredList(newList);
+  }
+
+  function updateSearch(message) {
+    filterList(message);
+  }
+
+  React.useLayoutEffect(() => {
+    navigation.dangerouslyGetParent().setOptions({
+      headerRight: () => <Search callbackFunc={updateSearch} />,
+    });
+  }, []);
+
+  return (
+    <>
+      <FlatList
+        data={filteredList}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            activeOpacity={0.6}
+            onPress={() =>
+              navigation.navigate('NewsArticle', {
+                author: item.author,
+                date: item.date,
+                title: item.title,
+              })
+            }
+          >
+            <NewsItem
+              author={item.author}
+              date={item.date}
+              title={item.title}
+            />
+          </TouchableOpacity>
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        ItemSeparatorComponent={() => <View style={styles.spacer} />}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  spacer: {
+    width: '100%',
+    height: 3,
+    backgroundColor: '#2A9D8F',
+  },
+});

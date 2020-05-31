029423f620b57044937b5d938022de4adc68d16f
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-30 21:17:16 -0300

    Add tab navigation

--- App.js
+++ App.js
@@ -9,19 +9,31 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Button, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './src/screens/Home';
+import NewsArticle from './src/screens/NewsArticle';
+import Management from './src/screens/Management';
 
 const Stack = createStackNavigator();
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
+  function HomeTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Management" component={Management} />
+      </Tab.Navigator>
+    );
+  }
   return (
     <>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen
-            name="Home"
-            component={Home}
+            name="home"
+            component={HomeTabs}
             options={{
               title: 'Home',
               headerStyle: {
@@ -29,9 +41,16 @@ const App = () => {
               },
               headerTintColor: '#fff',
               headerTitleStyle: {
-                textAlign: 'center',
+                alignSelf: 'center',
               },
+              headerLeft: () => <></>, // Just to make sure that the title will be centered
+              headerRight: () => <></>,
             }}
+          />
+          <Stack.Screen
+            name="NewsArticle"
+            component={NewsArticle}
+            options={{ headerShown: false }}
           />
         </Stack.Navigator>
       </NavigationContainer>

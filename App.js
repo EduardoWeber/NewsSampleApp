d1e54c8eb8a3f0c60a7dcc7e4a7011ee7a05a824
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-06-01 12:11:21 -0300

    Add tab navigation

--- App.js
+++ App.js
@@ -10,9 +10,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Entypo';
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './src/screens/Home';
 import NewsArticle from './src/screens/NewsArticle';
 import Management from './src/screens/Management';
+import Edit from './src/screens/Edit';
 
 const Stack = createStackNavigator();
 
@@ -21,7 +24,30 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   function HomeTabs() {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              return <Icon name="home" size={size} color={color} />;
+            }
+            if (route.name === 'Management') {
+              return (
+                <IconMCI name="file-document-edit" size={size} color={color} />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'gray',
+          style: {
+            backgroundColor: '#264653',
+            borderTopColor: 'transparent',
+          },
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Management" component={Management} />
       </Tab.Navigator>
@@ -30,7 +56,20 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#264653',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              alignSelf: 'center',
+            },
+            // headerLeft: () => <></>, // Just to make sure that the title will be centered
+            headerRight: () => <></>,
+          }}
+        >
           <Stack.Screen
             name="home"
             component={HomeTabs}
@@ -51,6 +90,11 @@ const App = () => {
             name="NewsArticle"
             component={NewsArticle}
             options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Edit"
+            component={Edit}
+            options={{ title: 'Editando' }}
           />
         </Stack.Navigator>
       </NavigationContainer>

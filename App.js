3025d124da67edc0af7e794265974651eb593414
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-06-02 14:41:06 -0300

    Add status bar color

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -54,6 +55,7 @@ const App = () => {
   }
   return (
     <>
+      <StatusBar backgroundColor="#264653" barStyle="ligth-content" />
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
